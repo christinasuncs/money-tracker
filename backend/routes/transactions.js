00f6e42ea0e69908e3b14ec5225eb14880aad68f
Christina Sun <christina.retune@gmail.com>
2025-11-06 19:25:35 -0500

    transactions endpoints done create, wip update

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,6 +1,6 @@
-// get 1 transaction
-// get all transaction that belong to 1 account
-// create new transaction
+// get 1 transaction - not needed yet
+// get all transaction that belong to 1 account - done
+// create new transaction - done
 // update a transaction
 // remove a transaction
 
@@ -31,5 +31,67 @@ module.exports = (supabase) => {
             return res.status(500).json({error: 'Internal server error'})
         }
     })
+    router.post('/', async (req, res) => {
+        if (!req.session.user) { // check if user is logged in
+            return res.status(401).json({ error: 'Not logged in'})
+        }
+        const { amount, notes, transaction_date, category } = req.body
+        const account_id = req.session.user.id
+        if (!amount || !notes || !transaction_date) {
+            return res.status(400).json({error: 'Missing required fields. Please provide amount, note, and transaction date.'})
+        }
+        try {
+            const { data, error } = await supabase
+                .from('Transactions')
+                .insert({ 
+                    account_id, 
+                    amount,
+                    notes,
+                    transaction_date,
+                    category
+                })
+                .select()
+            if (error) {
+                console.error("Error creating transaction: ", error)
+                return res.status(500).json({error: "Failed to create transactions."})
+            }
+            res.status(201).json({message: "Transaction created successfully", transaction: data})
+        } catch (error) {
+            console.error('Server error: ', error)
+            return res.status(500).json({error: 'Internal server error'})
+        }
+    })
+    router.put('/:transactionId', async (req, res) => {
+        if (!req.session.user) { // check if user is logged in
+            return res.status(401).json({ error: 'Not logged in'})
+        }
+        const { amount, notes, transaction_date, category } = req.body
+        const account_id = req.session.user.id
+        const transactionId = req.params.transactionId
+        if (!amount || !notes || !transaction_date) {
+            return res.status(400).json({error: 'Invalid fields entered. Please provide amount, note, and transaction date.'})
+        }
+        try {
+            const { data, error } = await supabase
+                .from('Transactions')
+                .update({ 
+                    account_id, 
+                    amount,
+                    notes,
+                    transaction_date,
+                    category
+                 })
+                .eq('id', transactionId)
+                .select()
+            if (error) {
+                console.error("Error updating transaction: ", error)
+                return res.status(500).json({error: "Failed to update transactions."})
+            }
+            res.status(201).json({message: "Transaction updated successfully", transaction: data})
+        } catch (error) {
+            console.error('Server error: ', error)
+            return res.status(500).json({error: 'Internal server error'})
+        }
+    }) 
     return router
 }
